0454affea02a7e7a77dcaed0b1c2a0b7f65f46d5
Guillem Andreu <git@guillemandreu.com>
2022-05-29 12:37:51 +0200

    build: tweak headers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,53 @@
+const contentSecurityPolicy = `
+  default-src 'self';
+  font-src assets.guillemandreu.com;
+`
+
+const securityHeaders = [
+  {
+    source: '/:path*',
+    headers: [
+      {
+        key: 'Content-Security-Policy',
+        value: contentSecurityPolicy.replace(/\s{2,}/g, ' ').trim(),
+      },
+      {
+        key: 'Referrer-Policy',
+        value: 'strict-origin-when-cross-origin',
+      },
+      {
+        key: 'Strict-Transport-Security',
+        value: 'max-age=31536000; includeSubDomains; preload',
+      },
+      {
+        key: 'X-Content-Type-Options',
+        value: 'nosniff',
+      },
+      {
+        key: 'X-DNS-Prefetch-Control',
+        value: 'on',
+      },
+      {
+        key: 'X-Frame-Options',
+        value: 'DENY',
+      },
+    ],
+  },
+]
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  eslint: {
+    dirs: ['.'],
+  },
+  async headers () {
+    return [
+      ...securityHeaders,
+    ]
+  },
+  poweredByHeader: false,
   reactStrictMode: true,
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
