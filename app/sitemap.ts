6ac6caed50e2b45a269608d42681bbc6b5bb3d00
Guillem Andreu <git@guillemandreu.com>
2023-04-10 18:21:27 +0200

    feat: use file-based metadata for sitemap

---
+++ app/sitemap.ts
@@ -0,0 +1,47 @@
+import { MetadataRoute } from 'next'
+
+import { getAllProjectPaths } from '../lib/getData'
+import siteMetadata from '../lib/siteMetadata'
+
+export default async function sitemap (): Promise<MetadataRoute.Sitemap> {
+  const projectPaths = await getAllProjectPaths()
+
+  const paths = [
+    {
+      url: `${siteMetadata.url}/`,
+      lastModified: new Date(),
+      priority: '1.0',
+    },
+    {
+      url: `${siteMetadata.url}/about/`,
+      lastModified: new Date(),
+      priority: '0.9',
+    },
+    {
+      url: `${siteMetadata.url}/contact/`,
+      lastModified: new Date(),
+      priority: '0.9',
+    },
+    {
+      url: `${siteMetadata.url}/privacy-policy/`,
+      lastModified: new Date(),
+      priority: '0.7',
+    },
+    {
+      url: `${siteMetadata.url}/work/`,
+      lastModified: new Date(),
+      priority: '0.9',
+    },
+  ]
+
+  projectPaths.forEach((projectPath) => {
+    paths.push({
+      url: `${siteMetadata.url}/work/${projectPath}/`,
+      lastModified: new Date(),
+      priority: '0.8',
+    })
+  })
+
+  return paths
+    .sort((a, b) => Number(b.priority) - Number(a.priority))
+}
