e614f3f3da27934c280833d2e08f9644cc4d58df
Guillem Andreu <git@guillemandreu.com>
2023-04-10 18:00:32 +0200

    feat: use file -based metadata for robots.txt

---
+++ app/robots.ts
@@ -0,0 +1,31 @@
+import { MetadataRoute } from 'next'
+import { headers } from 'next/headers'
+
+import siteMetadata from '../lib/siteMetadata'
+
+export default function robots (): MetadataRoute.Robots {
+  const headersList = headers()
+  const host = headersList.get('host')
+
+  if (host === siteMetadata.host) {
+    return {
+      rules: [
+        {
+          userAgent: '*',
+          allow: '/',
+        },
+      ],
+      sitemap: `${siteMetadata.url}/sitemap.xml`,
+      host: siteMetadata.host,
+    }
+  }
+
+  return {
+    rules: [
+      {
+        userAgent: '*',
+        disallow: '/',
+      },
+    ],
+  }
+}
