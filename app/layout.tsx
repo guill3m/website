6f816515dd0bcc4ff105d495deafb896dd92d119
Guillem Andreu <git@guillemandreu.com>
2023-09-11 22:50:14 +0200

    feat: move icons to app folder

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,27 +15,6 @@ export const metadata: Metadata = {
     name: 'Guillem Andreu',
     url: siteMetadata.url,
   },
-  icons: {
-    icon: [
-      {
-        url: '/favicon-32x32.png',
-        sizes: '32x32',
-        type: 'image/png',
-      },
-      {
-        url: '/favicon-16x16.png',
-        sizes: '16x16',
-        type: 'image/png',
-      },
-    ],
-    other: [
-      {
-        rel: 'mask-icon',
-        url: '/safari-pinned.tab.svg',
-        // color: '#f68d2e',
-      },
-    ],
-  },
   metadataBase: new URL(siteMetadata.url),
   openGraph: {
     locale: 'en',
