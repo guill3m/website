47164b1f3cf763fff4f6d7405d714f63166c4bd4
Guillem Andreu <git@guillemandreu.com>
2025-02-16 12:59:25 +0100

    feat: lastmod and priority in sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,36 @@
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
 
+const site = 'https://www.guillemandreu.com'
+
 export default defineConfig({
-	integrations: [sitemap()],
-	site: 'https://www.guillemandreu.com',
+	integrations: [
+		sitemap({
+			lastmod: new Date(),
+			serialize(item) {
+				switch (item.url) {
+					case `${site}/`:
+						item.priority = 1.0
+						break
+					case `${site}/about/`:
+					case `${site}/contact/`:
+					case `${site}/work/`:
+						item.priority = 0.9
+						break
+					case `${site}/ai/`:
+						item.priority = 0.7
+						break
+					case `${site}/privacy-policy/`:
+						item.priority = 0.6
+						break
+					default:
+						item.priority = 0.8
+						break
+				}
+				return item
+			},
+		}),
+	],
+	site,
 	trailingSlash: 'always',
 })
