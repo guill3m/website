203919d0f2585289eeb71cc4bc2f0f9f3a2e3af2
Guillem Andreu <git@guillemandreu.com>
2025-08-24 21:53:32 +0200

    build: remove unnecessary file
    this is now handled by astro:env

--- src/env.d.ts
+++
@@ -1,9 +0,0 @@
-interface ImportMetaEnv {
-	readonly UMAMI_ANALYTICS_HOST: string
-	readonly UMAMI_ANALYTICS_ID: string
-	readonly UMAMI_ANALYTICS_SCRIPT: string
-}
-
-interface ImportMeta {
-	readonly env: ImportMetaEnv
-}
